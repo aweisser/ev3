fb6249ce86edbaef1dd2ff7ceacce40dd8219c53
aweisser <armin.weisser@gmail.com>
2017-05-17 00:19:28 +0200

    =added environmental map and orientation

--- robot/robot.go
+++ robot/robot.go
@@ -2,20 +2,74 @@ package robot
 
 import (
 	"fmt"
+	"strings"
 )
 
-// SpeechModule enables speech abilities
-type SpeechModule interface {
+// The Direction of the robot
+type Direction int
+
+const (
+	North Direction = iota
+	East
+	South
+	West
+)
+
+func (d Direction) String() string {
+	switch d {
+	case North:
+		return "▲"
+	case East:
+		return "▶"
+	case South:
+		return "▼"
+	case West:
+		return "◀"
+	default:
+		return "X"
+	}
+}
+
+// Position of the robot in the environmental map including Orientation
+type Position struct {
+	X           int
+	Y           int
+	Orientation Direction
+}
+
+// Speaker enables speech abilities
+type Speaker interface {
 	Say(text string)
+}
+
+// Printer enables printing abilities
+type Printer interface {
+	Print(text string)
 }
 
 // Robot represents an executable EV3 device
 type Robot struct {
 	Name         string
-	SpeechModule SpeechModule
+	EnvMap       string
+	Position     Position
+	SpeechModule Speaker
+	PrintModule  Printer
 }
 
 // Greet with your name
 func (r *Robot) Greet() {
 	r.SpeechModule.Say(fmt.Sprintf("Hi. My name is %v.", r.Name))
+}
+
+// PrintEnvironment prints the current environment and the position of the robot
+func (r *Robot) PrintEnvironment() {
+	lines := strings.Split(r.EnvMap, "\n")
+	offset := 0
+	if lines[0] == "" {
+		offset = 1
+	}
+	line := lines[r.Position.Y]
+	newLine := fmt.Sprintf("%v%v%v", line[0:r.Position.X+1], r.Position.Orientation.String(), line[r.Position.X+2:len(line)])
+	lines[r.Position.Y+offset] = newLine
+	r.PrintModule.Print(strings.Join(lines, "\n"))
 }
--- robot/robot_test.go
+++ robot/robot_test.go
@@ -3,16 +3,24 @@ package robot
 import "testing"
 import "fmt"
 
-type MockSpeechModule struct {
-	t    *testing.T
-	said string
+type Mock struct {
+	t        *testing.T
+	expected string
 }
 
-func (s MockSpeechModule) Say(text string) {
+func (mock *Mock) verify(text string) {
 	fmt.Println(text)
-	if s.said != text {
-		s.t.Errorf("Expected %v but was %v", s.said, text)
+	if mock.expected != text {
+		mock.t.Errorf("Expected %v but was %v", mock.expected, text)
 	}
+}
+
+func (mock *Mock) Say(text string) {
+	mock.verify(text)
+}
+
+func (mock *Mock) Print(text string) {
+	mock.verify(text)
 }
 
 func Test_TwoRobotsShouldHaveDifferentNames(t *testing.T) {
@@ -28,6 +36,33 @@ func Test_TwoRobotsShouldHaveDifferentNames(t *testing.T) {
 
 func Test_RobotShouldHaveGreetFunction(t *testing.T) {
 	r := Robot{Name: "EV3"}
-	r.SpeechModule = MockSpeechModule{t: t, said: "Hi. My name is EV3."}
+	r.SpeechModule = &Mock{t: t, expected: "Hi. My name is EV3."}
 	r.Greet()
+}
+
+func Test_DirectionNorthShouldBePrintable(t *testing.T) {
+	if North.String() != "▲" {
+		t.Errorf("Expected %v but was %v", "▲", North)
+	}
+}
+
+func Test_RobotShouldByAbleToLocateItselfOnAnEnvironmentalMap(t *testing.T) {
+	r := Robot{}
+	r.EnvMap = `
+###########
+#         #
+#         #
+#         #
+###########`
+	r.Position = Position{X: 6, Y: 3, Orientation: North}
+
+	expectedMap := `
+###########
+#         #
+#         #
+#      ▲  #
+###########`
+	r.PrintModule = &Mock{t: t, expected: expectedMap}
+
+	r.PrintEnvironment()
 }
